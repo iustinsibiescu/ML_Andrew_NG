051f6a4b6d6044d70aa983e433366c33a135a748
Iustin Sibiescu <sibiescuiustin@gmail.com>
2018-03-14 22:52:17 -0700

    Finished week7

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -7,28 +7,27 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+% Useful variables
+paramRange = [0.01 0.03 0.1 0.3 1 3 10 30];
+lengthParamRange = length(paramRange);
+minimumPredictionError = intmax('int32');
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
-
-
-
-
-
-
-
-% =========================================================================
+% Choosing the best parameters
+for i = 1 : lengthParamRange
+    for j = 1 : lengthParamRange
+        C_train = paramRange(i);
+        sigma_train = paramRange(j);
+        
+        model= svmTrain(X, y, C_train, @(x1, x2) gaussianKernel(x1, x2, sigma_train));
+        predictions = svmPredict(model, Xval);
+        predictionError = mean(double(predictions ~= yval));
+        
+        if minimumPredictionError > predictionError
+            C = C_train;
+            sigma = sigma_train;
+            minimumPredictionError = predictionError;
+        end
+    end
+end
 
 end
