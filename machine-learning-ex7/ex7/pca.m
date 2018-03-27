a2e282c852226add4843457e6d8cd737c9d6d78f
Iustin Sibiescu <sibiescuiustin@gmail.com>
2018-03-27 01:06:11 -0700

    Finished week 8

--- machine-learning-ex7/ex7/pca.m
+++ machine-learning-ex7/ex7/pca.m
@@ -11,21 +11,10 @@ function [U, S] = pca(X)
 U = zeros(n);
 S = zeros(n);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should first compute the covariance matrix. Then, you
-%               should use the "svd" function to compute the eigenvectors
-%               and eigenvalues of the covariance matrix. 
-%
-% Note: When computing the covariance matrix, remember to divide by m (the
-%       number of examples).
-%
+% Computing the covariance matrix
+sigma = X' * X / m;
 
-
-
-
-
-
-
-% =========================================================================
+% Obtaining the eigenvectors and eigenvalues of the covariance matrix
+[U, S, V] = svd(sigma);
 
 end
